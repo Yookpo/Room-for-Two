22bfc5304e103683f19e48c3b70d48b16a6edb27
HJaeH08 <ghkdwogml100@naver.com>
2025-06-29 12:18:27 +0900

    logger구현

---
+++ Room-For-Two/Logger/Logger.cpp
@@ -0,0 +1,86 @@
+﻿#include "Logger.h"
+
+#include<sstream>
+
+
+#include<mutex>
+
+
+
+
+
+
+void Log::Logger::RegisterCallback(std::function<void(const std::string&)> callback)
+{
+
+	{
+		std::lock_guard<std::mutex> lock_guard(LogMutex);
+		CallbackVector.push_back(std::move(callback));
+	}
+
+}
+
+bool Log::Logger::IsValid() const
+{
+	return ValidState;
+}
+
+Log::Logger::Logger()
+	: ValidState(false)
+{
+	
+	ToLogLevelStringTable[ELogLevel::Warning] = "[Warning]";
+	ToLogLevelStringTable[ELogLevel::Error] = "[Error]";
+	ToLogLevelStringTable[ELogLevel::Critical] = "[Critical]";
+	ToLogLevelStringTable[ELogLevel::Info] = "[Info]";
+
+}
+
+Log::Logger& Log::Logger::GetInstance()
+{
+	static Logger log;
+
+	return log;
+}
+
+Log::Logger::~Logger()
+{
+
+
+}
+
+bool Log::Logger::Initialize(const std::string& outputFile)
+{
+	//스레드 안정성 확보할것
+
+	Ofstream.open(outputFile);
+	if (!Ofstream.is_open())
+	{
+		//파일열기실패;
+		return false;
+	}
+	ValidState = true;
+	return true;
+}
+
+void Log::Logger::Log(ELogLevel level, const std::string& category, const std::string& message)
+{
+	
+	std::stringstream log;
+		
+	log << ToLogLevelStringTable[level] << "[" << category <<"]" << ":" << message << "\n";
+
+
+	{
+		std::lock_guard<std::mutex> lock_guard(LogMutex);
+
+		Ofstream.write(log.str().c_str(), log.str().size());
+		for (auto& callback : CallbackVector)
+		{
+			callback(log.str());
+		}
+
+	}
+
+
+}
---
+++ Room-For-Two/Logger_Test/main.cpp
@@ -0,0 +1,30 @@
+﻿
+
+
+
+
+#include<Logger.h>
+#include<iostream>
+
+int main()
+{
+
+	Log::Logger  & logger = Log::Logger::GetInstance();
+
+	bool initResult = logger.Initialize("LogFile.txt");
+
+	if (!initResult)
+	{
+		std::cout << "파일열기 실패";
+		return 0 ;
+	}
+
+
+	logger.RegisterCallback([](const std::string& log) { std::cout << log; });
+
+	logger.Log(Log::ELogLevel::Error, "Asset", "해당Asset이 유효하지않아 올바르게 작동되지않았습니다.");
+	logger.Log(Log::ELogLevel::Critical, "Render", "렌더시스템코어에서 Gpu리소스가 유효하지않았습니다.");
+
+
+	return 0;
+}
